e51d365ba13a36eb324ea0be436c06e4646cc4b3
hemantmudgal <hemant.mudgal_cs19@gla.ac.in>
2021-11-17 20:38:10 +0530

    added PostWithComment Component

--- src/Pages/Dashboard.page.tsx
+++ src/Pages/Dashboard.page.tsx
@@ -1,9 +1,12 @@
 import { FC, memo } from "react";
+import PostWithComment from "../component/PostWithComment";
 
 interface Props {}
 
 const Dashboard: FC<Props> = (props) => {
-  return <div>dashboard page.</div>;
+  return <div className="m-10">
+    <PostWithComment />
+  </div>;
 };
 
 Dashboard.defaultProps = {};
